57b38caef66d087958a189d194e4c75399b3f30f
skociuba <skociuba83@gmail.com>
2024-01-26 09:58:22 +0100

    fix2

--- src/app/components/Hero.tsx
+++ src/app/components/Hero.tsx
@@ -4,7 +4,7 @@ const Hero: React.FC = () => (
   <section id="home">
     <div className="custom-img mb-12 flex h-screen items-center justify-center bg-cover bg-fixed bg-center">
       {/* Overlay */}
-      <div className="text-custom-blue fixed inset-0 bg-black/70" />
+      <div className="fixed inset-0 bg-black/70 text-custom-blue" />
       <div className="z-[2] p-5 text-white">
         <h2 className="text-2xl font-bold lg:text-5xl">
           We are what we repeat in our lives.
@@ -14,14 +14,14 @@ const Hero: React.FC = () => (
 
         <div className="mt-12 flex flex-col justify-start md:flex-row">
           <Link href="/portfolio">
-            <button className="hover:bg-custom-blue text-custom-blue mb-4 mt-1 block h-12 w-[300px] rounded border hover:text-black md:mb-0">
+            <button className="mb-4 mt-1 block h-12 w-[300px] rounded border text-custom-blue hover:bg-custom-blue hover:text-black md:mb-0">
               Portfolio{' '}
             </button>
           </Link>
 
           <Link href="/sendEmail">
             {' '}
-            <button className="bg-custom-blue hover:text-custom-blue ml-0 mt-1 block h-12 w-[300px] rounded border text-black hover:bg-inherit lg:ml-5 ">
+            <button className="ml-0 mt-1 block h-12 w-[300px] rounded border bg-custom-blue text-black hover:bg-inherit hover:text-custom-blue lg:ml-5 ">
               Contact Me{' '}
             </button>
           </Link>
